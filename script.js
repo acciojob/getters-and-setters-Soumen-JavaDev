3bcc3e528485d5628d2649428a1dfc7bfd839dde
SOUMEN MONDAL <151416831+Soumen-JavaDev@users.noreply.github.com>
2024-10-30 20:18:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age){
+		this._name=name;
+		this._age=age;
+	}
+	get name(){
+		return this._name;
+	}
+	set age(age){
+		this._age=age;
+	}
+	get age(){
+		return this._age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
 
-class Teacher extends Person {}
+	 study() {
+        console.log(`${this.name} is studying`);
+    }
+}
+
+class Teacher extends Person {
+	 teach() {
+        console.log(`${this.name} is teaching`);
+    }
+}
+const person = new Person("John", 25);
+
+
 
 // Do not change the code below this line
 window.Person = Person;
